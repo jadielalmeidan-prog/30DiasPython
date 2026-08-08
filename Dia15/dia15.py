060a24b48302e200f0a13a53d45d9d545a3b2564
jadielalmeidan-prog <jadielalmeidan@gmail.com>
2026-08-08 11:00:35 -0300

    Add files via upload

---
+++ Dia15/dia15.py
@@ -0,0 +1,23 @@
+#print "Seiya é um otimo personagem" Erro:SyntaxError.
+
+#print(nome) Erro: NomeError.
+
+#numbers = [1, 2, 3, 4, 5]
+
+#print(numbers[6])
+
+#import pypdfs Erro: ModuleNotFoundError.
+
+#import math
+
+#math.PI Erro: AtributeError.
+
+#users = {'name':'Asab', 'age':250, 'country':'Finland'}
+
+#print(users['idade']) Erro: KeyError.
+
+#from math import power Erro: ImportError.
+
+#float("ia1.3") Erro: ValueError.
+
+#print(13/0) Erro: ZeroDivisionError.
